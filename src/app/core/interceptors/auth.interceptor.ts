5b57f9f6cee74dcdd49a4735715e4c5ab7a442eb
Mahmood Elbadri <mahmoud.elbadry357@gmail.com>
2026-03-31 23:01:41 +0200

    feat: add auth interceptor to inject JWT token and handle 401 unauthorized errors

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,17 +1,35 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
+  const router = inject(Router);
+  
+  let clonedReq = req;
   if (token) {
-    req = req.clone({
+    clonedReq = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
   }
   //in case of token expired
+
+
   
   
-  
-  return next(req);
+  return next(clonedReq).pipe(
+    catchError((err)=>{
+      if(err.status === 401){
+        localStorage.removeItem('token');
+        localStorage.removeItem('role');
+        localStorage.removeItem('userName');
+        localStorage.removeItem('email');
+        router.navigate(['/login']);
+      }
+      return throwError(()=> new Error("You don't have permission to access this resource"));
+    })
+  )
 };
